5604a9bd75934954c7c8e52343c2008e7a4e0d04
shradha1612 <shradhaparkhi123@gmail.com>
2020-05-23 12:49:06 +0530

    Updates

---
+++ precache-manifest.01b073136a8c798bbffdab32b4628f47.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147005903e29ff25041dcdbd5a64e48f",
+    "url": "/blogpost/index.html"
+  },
+  {
+    "revision": "73784fc020e22664fde3",
+    "url": "/blogpost/static/css/main.85130128.chunk.css"
+  },
+  {
+    "revision": "28ad7947a5233e5ff8ed",
+    "url": "/blogpost/static/js/2.0673d227.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blogpost/static/js/2.0673d227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73784fc020e22664fde3",
+    "url": "/blogpost/static/js/main.787bad9a.chunk.js"
+  },
+  {
+    "revision": "e7962df424d40088712e",
+    "url": "/blogpost/static/js/runtime-main.d1834c8d.js"
+  },
+  {
+    "revision": "d09c13de9b14737fb152101dc6dc2ac9",
+    "url": "/blogpost/static/media/home-office.d09c13de.jpg"
+  },
+  {
+    "revision": "6d031d687adb745a36f36edcc2a72e97",
+    "url": "/blogpost/static/media/javascript.6d031d68.jpg"
+  },
+  {
+    "revision": "76f2e0f753d95eeaf74a34febae8ef1a",
+    "url": "/blogpost/static/media/learntocode.76f2e0f7.jpg"
+  },
+  {
+    "revision": "15c589deb4726afd4a82898985e0b38e",
+    "url": "/blogpost/static/media/shradha DP.15c589de.jpg"
+  },
+  {
+    "revision": "9c53b0fdad6663b20cbf18323fc1ffb0",
+    "url": "/blogpost/static/media/slider.9c53b0fd.jpg"
+  },
+  {
+    "revision": "2db5d8e9d30d0df962000f69faa3e40e",
+    "url": "/blogpost/static/media/startup-593341_640.2db5d8e9.jpg"
+  },
+  {
+    "revision": "6ad8d0a436fcaec39b8ce73a941b94ce",
+    "url": "/blogpost/static/media/tech.6ad8d0a4.jpg"
+  }
+]);
